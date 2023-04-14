043cf5fb8da54fc050c99828e085df91372fb6f0
trustmitt <mainmirror14@gmail.com>
2023-04-14 22:57:20 +0900

    feat : 서버 사이드 렌더링 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Seo from "@/components/Seo";
 
-export default function Home() {
-    const [movies, setMovies] = useState<any[]>([]);
-
-    useEffect(() => {
-        (async () => {
-            const { results } = await (await fetch(`/api/movies`)).json();
-            setMovies(results);
-            console.log(movies);
-        })();
-    }, []);
-
+export default function Home({ results }: any) {
     return (
         <div className="container">
             <Seo title="Home" />
-            {!movies.length && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
+
+            {results?.map((movie: any) => (
                 <div className="movie" key={movie.id}>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
@@ -45,4 +35,13 @@ export default function Home() {
             `}</style>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    return {
+        props: {
+            results,
+        },
+    };
 }
