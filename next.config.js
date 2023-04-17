b1edbbb4b1e86c425b47ee03f9a9a091573481c7
trustmitt <mainmirror14@gmail.com>
2023-04-17 20:27:09 +0900

    feat : detail 페이지 상단 영역 구현

--- next.config.js
+++ next.config.js
@@ -2,24 +2,18 @@
 const API_KEY = process.env.API_KEY;
 const nextConfig = {
     reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: "/contact",
-                destination: "/form",
-                permanent: false,
-            },
-        ];
+    images: {
+        domains: ["image.tmdb.org"],
     },
     async rewrites() {
         return [
             {
                 source: "/api/movies",
-                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&region=KR`,
             },
             {
                 source: "/api/movies/:id",
-                destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+                destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&language=ko-KR`,
             },
         ];
     },
--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,21 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import Seo from "@/components/@common/Seo";
-import { useRouter } from "next/router";
-import React from "react";
 
 export default function Detail({ params }: any) {
-    const router = useRouter();
-    console.log(router);
     const [title, id]: any = params || [];
+    const [movie, setMovie] = useState<any>([]);
+
+    const fetchMovieDetails = async () => {
+        const result = await (await fetch(`https://movol.vercel.app/api/movies/${id}`)).json();
+        setMovie(result);
+        console.log(result);
+    };
+
+    useEffect(() => {
+        fetchMovieDetails();
+        console.log(movie);
+    }, []);
 
     return (
         <div>
             <Seo title={title} />
-            <h4>{title}</h4>
+            <div className="flex justify-center py-20">
+                <div>
+                    <figure className="relative w-[300px] h-[500px] md:w-[440px] lg:w-[560px]">
+                        <Image src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.original_title} className="rounded-lg object-cover" fill />
+                    </figure>
+                </div>
+                <div className="ml-4">
+                    <h2>{title}</h2>
+                    <div>{movie.tagline}</div>
+                    {movie.genres && (
+                        <div className="flex">
+                            {movie.genres.map((e: { id: number; name: string }) => (
+                                <div>{e.name}</div>
+                            ))}
+                        </div>
+                    )}
+                    <div>{movie.release_date}</div>
+                    <div>{movie.overview}</div>
+                </div>
+            </div>
         </div>
     );
 }
 
-export function getServerSideProps({ params: { params } }: any) {
+export async function getServerSideProps({ params: { params } }: any) {
     return {
         props: {
             params,
