539fddd9dbafb115b99a81c315b34ab242e97fa3
trustmitt <mainmirror14@gmail.com>
2023-04-19 03:00:15 +0900

    fix : 랜딩 페이지 스크롤 애니메이션 변경

--- components/root/TopMovies.tsx
+++ components/root/TopMovies.tsx
@@ -15,7 +15,7 @@ export default function TopMovies({ results }: TypeResult) {
 
     return (
         <div className="py-40 px-12 min-h-screen">
-            <Fade duration={1000} direction={"down"} cascade={true}>
+            <Fade duration={1000} cascade={true}>
                 <div className="text-4xl sm:text-5xl text-center font-extrabold mt-16 text-black">TOP MOVIE 8</div>
                 <div className="text-xl text-center mt-4 text-black">This is a list of the most popular movies at the moment.</div>
                 <div className="container mx-auto flex flex-wrap items-start py-16">
--- pages/gacha.tsx
+++ pages/gacha.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
 import { useRouter } from "next/router";
+import { Fade } from "react-awesome-reveal";
 import Seo from "@/components/@common/Seo";
 
 export default function Gacha() {
@@ -16,8 +16,8 @@ export default function Gacha() {
     const fetchGachaMovie = async () => {
         const num = getRandom(1, 300);
         const gachaMovie = await (await fetch(`https://movol.vercel.app/api/list/${num}`)).json();
-        setResult(gachaMovie.results);
         console.log(result);
+        setResult(gachaMovie.results);
     };
 
     useEffect(() => {
@@ -27,25 +27,20 @@ export default function Gacha() {
     return (
         <div>
             <Seo title="About" />
-            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-                    </p>
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <a
-                            href="#"
-                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Get started
-                        </a>
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                            Learn more <span aria-hidden="true">→</span>
-                        </a>
+            <Fade duration={1000} cascade={true}>
+                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                    <div className="text-center">
+                        <h1 className="text-3xl px-12 font-bold tracking-tight text-gray-900 sm:text-5xl">If you're thinking about what movie to watch</h1>
+                        <p className="mt-6 text-xs leading-8 text-gray-900 sm:text-sm">When you click the button, Movol automatically selects the movie.</p>
+                        <p className="text-xs leading-8 text-gray-900 sm:text-sm">Click the results to go to the Movie Details page.</p>
+                        <div className="mt-6 flex items-center justify-center gap-x-6">
+                            <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                Get started
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fade>
         </div>
     );
 }
