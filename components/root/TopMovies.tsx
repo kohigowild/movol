81e790cd8f1735b1e357bd5170b76bb77aef65ff
trustmitt <mainmirror14@gmail.com>
2023-04-19 02:20:28 +0900

    chore : react-reveal 패키지 변경 및 sharp 설치

--- components/root/TopMovies.tsx
+++ components/root/TopMovies.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { TypeResult } from "@/interface/movie";
-import Slide from "react-reveal/Slide";
+import { Fade } from "react-awesome-reveal";
 
 export default function TopMovies({ results }: TypeResult) {
     const router = useRouter();
@@ -14,15 +14,17 @@ export default function TopMovies({ results }: TypeResult) {
     };
 
     return (
-        <div>
-            <div className="text-5xl text-center font-extrabold mt-16 text-black">TOP MOVIE 8</div>
-            <div className="text-xl text-center mt-4 text-black">This is a list of the most popular movies at the moment.</div>
+        <div className="py-40 px-12 min-h-screen">
+            <Fade duration={1000} direction={"down"} cascade={true}>
+                <div className="text-5xl text-center font-extrabold mt-16 text-black">TOP MOVIE 8</div>
+                <div className="text-xl text-center mt-4 text-black">This is a list of the most popular movies at the moment.</div>
+            </Fade>
             <div className="container mx-auto flex flex-wrap items-start py-16 px-16">
                 {results?.slice(0, 8).map((movie: any) => (
-                    <Slide bottom>
-                        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 pl-4 pr-4 mb-4 cursor-pointer" key={movie.id} onClick={() => handleItemClick(movie.id, movie.original_title)}>
+                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 pl-4 pr-4 mb-4 cursor-pointer" key={movie.id} onClick={() => handleItemClick(movie.id, movie.original_title)}>
+                        <Fade duration={1000} direction={"down"} cascade={true}>
                             <div className="rounded-lg m-h-64 p-2 transform hover:-translate-y-2 hover:shadow-2xl transition duration-300">
-                                <figure className="relative h-[240px]">
+                                <figure className="relative h-[200px]">
                                     <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} className="ml-auto mr-auto rounded-t-lg object-cover" fill />
                                 </figure>
                                 <div className="rounded-b-lg p-4 backdrop-blur-md bg-white/30 flex flex-col">
@@ -32,8 +34,8 @@ export default function TopMovies({ results }: TypeResult) {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </Slide>
+                        </Fade>
+                    </div>
                 ))}
             </div>
         </div>
